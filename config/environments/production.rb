d0e479c42ada2f80068a4879e4377e25405605a8
Álvaro Artano <alvaroartano@gmail.com>
2025-12-06 19:40:52 +0100

    fixed smtp issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,10 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :address   => 'smtp.resend.com',
-    :port      => 465,
+    :port      => 587,
     :user_name => ENV["SMTP_USERNAME"],
     :password  => ENV['SMTP_PASSWORD'],
-    :tls => true
+    :enable_starttls_auto => true
   }
 
   # Code is not reloaded between requests.
